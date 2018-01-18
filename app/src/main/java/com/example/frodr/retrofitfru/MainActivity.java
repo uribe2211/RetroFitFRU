d721320c784f920bb49bd32a10e71d4686e5c2f4
Felipe Rodriguez <uribe2211@hotmail.com>
2018-01-18 15:41:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/frodr/retrofitfru/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.frodr.retrofitfru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
